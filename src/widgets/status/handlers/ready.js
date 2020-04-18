256a973c6fda6ac4db7248fec49d66408ca4c315
Muazam Kamal <muazam.kamal@gmail.com>
2020-04-18 13:33:59 +0800

    Removed status submodule

--- src/widgets/status/handlers/ready.js
+++
@@ -1,9 +0,0 @@
-const { status, activityType } = require("../config");
-
-module.exports = async client => {
-    console.log("status: ready");
-
-    client.user.setActivity(status, { type: activityType })
-        .then(presence => console.log(`Activity set to \"${presence.activities[0].name}\", type \"${presence.activities[0].type}\"`))
-        .catch(console.error);
-};
